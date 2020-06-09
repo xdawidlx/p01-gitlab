e68c68437b1e278947ac24ee6db79ccc98bfd2bd
xdawidlx <66701956+xdawidlx@users.noreply.github.com>
2020-06-09 23:51:15 +0200

    Update 01program.php

--- 01program.php
+++ 01program.php
@@ -4,5 +4,5 @@ echo ("<br> Jestem programem 01");
 echo ("<br> Test 10101");
 echo ("<br> TEST 20202");
 echo ("<br>Dalsza czesc programu");
-
+echo ("<br>Dalsza czesc programu test 20202 z githuba");
 ?>
